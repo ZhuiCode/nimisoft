99218773ffbe023e91050c6318116494b774cb6e
ZhuiCode <zhui890530@outlook.com>
2017-09-14 23:30:39 +0800

    钱包页面和交易记录页面
    钱包页面和交易记录页面静态页面代码实现

--- pages/wallet/wallet.js
+++ pages/wallet/wallet.js
@@ -1,19 +1,29 @@
 var app=getApp();
 Page({
     data: {
-        remain_power: 0
+        remain_power: 0,
+        items: [
+          { fish_food: '50', value:'19.98'}, 
+          { fish_food: '100', value:'99'},
+          { fish_food: '200', value:'198'},
+          { fish_food: '300', value:'295'},
+        ],
+        pick_arr: ['50颗(优惠价:19.98元)', '100颗(优惠价:19.98元)', '200颗(优惠价:19.98元)','300颗(优惠价:19.98元)'],
+        index:1,
     },
     onLoad: function () {
         var that = this;
         that.setData({
-            'remain_power':app.globalData.remain_power       
-            })
-
+            'remain_power': app.globalData.remain_power,
+        });
     },
-    exchange:function()
-    {
+    bindPickerChange: function (e) {
+      
+      this.setData({ index: e.detail.value })
+    },
+    trans_record: function () {
         wx.navigateTo({
-          url: '../exchange/exchange'
+            url: '../trans_record/trans_record'
         })
     },
 })
